a8a19061f3247573620c436f5ee9c99a940b2e4d
starclee <sabarivijay214@gmail.com>
2025-02-01 17:37:31 +0530

    Handling games without img

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import gameCover from "../assets/Game cover.webp";
+
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return gameCover;
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   const croppedUrl = url.slice(0, index) + "crop/600/400/" + url.slice(index);
